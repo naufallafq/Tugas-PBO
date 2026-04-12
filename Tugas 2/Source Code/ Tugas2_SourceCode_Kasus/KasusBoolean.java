232dacde4beba27c37f0b43010499ae7ca05acd1
naufallafq <muh.naufal1903@gmail.com>
2026-04-12 22:23:19 +0800

    Create KasusBoolean.java

---
+++ Tugas 2/Source Code/ Tugas2_SourceCode_Kasus/KasusBoolean.java
@@ -0,0 +1,28 @@
+/* NIM   : 13020240243
+Nama  : M Naufal Al Afiq
+Hari/Tanggal : 11 April 2026
+Waktu : 13:00*/
+
+/* Eksrpesi kondisional dengan boolean */  
+public class KasusBoolean { 
+	
+	/** 
+	* @param args 
+	*/ 
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub 
+		/* Kamus */  
+		boolean bool; 
+		/* Algoritma */  
+		bool= true; 
+		if(bool) {  
+			System.out.print("true\n"); 
+		} else  
+			System.out.print("false\n");  
+		
+		if(!bool) {  
+			System.out.print("salah\n"); 
+		} else  
+			System.out.print("benar\n"); 
+	} 
+}
