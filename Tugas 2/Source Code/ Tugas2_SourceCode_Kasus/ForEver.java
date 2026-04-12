6a5b2d4e574fc43b2d48c941ab7d000d7cc9ab9c
naufallafq <muh.naufal1903@gmail.com>
2026-04-12 22:16:56 +0800

    Create ForEver.java

---
+++ Tugas 2/Source Code/ Tugas2_SourceCode_Kasus/ForEver.java
@@ -0,0 +1,21 @@
+
+/* NIM   : 13020240243
+Nama  : M Naufal Al Afiq
+Hari/Tanggal : 11 April 2026
+Waktu : 13:00*/
+
+public class ForEver {
+	
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Program */
+		System.out.println("Program akan looping, akhiri dengan ^c");
+		while (true)
+		{	System.out.print ("Print satu baris...\n");
+			break;
+		}
+	}
+}
